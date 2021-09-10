361dea76b9f6b88fcc6c7db789f9d26f81ff34e4
“[Зорин <[evgenijzorin@bk.ru]>
2021-09-10 22:44:05 +0300

    Create horyzont and vertycal lines classes

---
+++ Snake/Snake/HoryzontLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HoryzontLine
+    {
+        List<Point> pList; // поле
+
+        /// <summary>
+        ///  класс горизонтальная линия
+        /// </summary>
+        public HoryzontLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x<= xRight; x++)
+            {
+                Point p = new Point(x, y,sym);
+                pList.Add(p);
+            }
+        }
+        public void Draw ()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
---
+++ Snake/Snake/VertycalLine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class VertycalLine
+    {
+        List<Point> pList; // поле
+        /// <summary>
+        ///  класс вертикальная линия
+        /// </summary>
+        public VertycalLine(int yTop, int yBotom, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yTop; y <= yBotom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
