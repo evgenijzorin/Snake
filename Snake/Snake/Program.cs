cb3fe7bc01143b7da584fd452d2e5dc72fb037da
“[Зорин <[evgenijzorin@bk.ru]>
2021-09-10 22:40:36 +0300

    Create horyzont and vertycal lines

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -12,27 +12,13 @@ namespace Snake
             p1.Draw(); // вызов метода
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-            Point p3 = new Point(5, 12, 'r');
-            p3.Draw();
 
-            List<int> numList = new List<int>();
+            HoryzontLine line = new HoryzontLine(5,10,8, '+');
+            line.Draw();
+            VertycalLine vLine = new VertycalLine(2, 15,10, '*');
+            vLine.Draw();
 
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-            foreach(int i in numList)
-            {
-                Console.WriteLine(i);
-            }  
-            numList.RemoveAt(0);
 
-            List<Point> listPoint = new List<Point>();
-            listPoint.Add(p1);
-            listPoint.Add(p2);
-            listPoint.Add(p3);
 
 
 
